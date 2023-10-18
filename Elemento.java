1c0eac42828a376da23c3b428ed7428f6277fb99
André Rossi <andre_barros@estudante.sc.senai.br>
2023-10-17 21:11:16 -0300

    commit

---
+++ Elemento.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Elementos {
+    
+    static Random random = new Random();
+
+    private int poder;
+    private int ataque;
+    private int velocidade; 
+    private int defesa;
+    private int vida;
+
+} //classe Elementos
